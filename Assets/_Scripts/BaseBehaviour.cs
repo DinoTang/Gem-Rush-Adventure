16a0c2573654eb7f081ea5376e0f65cbeb398a2a
Khanh Tang <quockhanhtanghuynh@gmail.com>
2026-04-19 23:01:58 +0700

    Make boardmanager to spawn gem in scene

---
+++ Assets/_Scripts/BaseBehaviour.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public abstract class BaseBehaviour : MonoBehaviour
+{
+    protected virtual void Start()
+    {
+        this.LoadComponent();
+    }
+
+    protected virtual void Awake()
+    {
+        this.LoadComponent();
+    }
+
+    protected virtual void Reset()
+    {
+        this.ResetValue();
+        this.LoadComponent();
+    }
+    protected virtual void OnEnable()
+    {
+
+    }
+    protected virtual void LoadComponent()
+    {
+
+    }
+
+    protected virtual void ResetValue()
+    {
+
+    }
+}
---
+++ Assets/_Scripts/_Core/Match/MatchResolver.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+
+public class ResolveMatchResult
+{
+    public List<(int x, int y)> GetCellsToClear(List<MatchResult> matches)
+    {
+        HashSet<(int x, int y)> cellsToClear = new();
+
+        foreach (var match in matches)
+        {
+            foreach (var cell in match.Cells)
+            {
+                cellsToClear.Add((cell.x, cell.y));
+            }
+        }
+
+        List<(int x, int y)> result = new List<(int x, int y)>(cellsToClear);
+
+        return result;
+    }
+
+    public void ClearMatches(List<MatchResult> matches, GridModel grid)
+    {
+        List<(int x, int y)> result = GetCellsToClear(matches);
+        result.Sort();
+
+        foreach (var cell in result)
+        {
+            grid.Set(cell.x, cell.y, new Piece(PieceType.None));
+        }
+    }
+}
