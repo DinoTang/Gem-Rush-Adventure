08ec8c395df1c23a09a60dbce9477a7cb06ea9f2
Khanh Tang <quockhanhtanghuynh@gmail.com>
2026-04-23 17:18:50 +0700

    Use Coroutine to make anim when swap 2 gem together

---
+++ Assets/GemMove.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemMove : BaseBehaviour
+{
+    [SerializeField] protected GemCtrl gemCtrl;
+    public GemCtrl GemCtrl => gemCtrl;
+
+    protected override void LoadComponent()
+    {
+        base.LoadComponent();
+        this.LoadGemCtrl();
+    }
+
+    protected void LoadGemCtrl()
+    {
+        if (this.gemCtrl != null) return;
+        this.gemCtrl = transform.parent.GetComponent<GemCtrl>();
+        Debug.Log(transform.name + ": LoadGemCtrl");
+    }
+
+    public IEnumerator MoveTo(Vector3 target, float duration)
+    {
+        Vector3 start = transform.parent.localPosition;
+        float time = 0f;
+
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            float t = time / duration;
+
+            transform.parent.localPosition = Vector3.Lerp(start, target, t);
+            yield return null;
+        }
+
+        transform.parent.localPosition = target;
+    }
+}
