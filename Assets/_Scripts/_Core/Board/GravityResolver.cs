62e0e9253e91c770e5ec65c8b77998778e0e02c6
Khanh Tang <quockhanhtanghuynh@gmail.com>
2026-04-25 16:46:30 +0700

    WIP: start implementing animated gravity and refill fall system

--- Assets/_Scripts/_Core/Board/GravityResolver.cs
+++ Assets/_Scripts/_Core/Board/GravityResolver.cs
@@ -1,9 +1,18 @@
+using System.Collections.Generic;
 using UnityEngine;
+
+public class FallMove
+{
+    public GemCtrl gem;
+    public Vector3 currentPos;
+    public Vector3 targetPos;
+}
 
 public class GravityResolver
 {
-    public void ApplyGravity(GridModel<GemCtrl> grid)
+    public List<FallMove> ApplyGravity(GridModel<GemCtrl> grid)
     {
+        List<FallMove> fallMoves = new();
         for (int x = 0; x < grid.Width; x++)
         {
             for (int y = grid.Height - 1; y >= 0; y--)
@@ -18,16 +27,23 @@ public class GravityResolver
                     if (fallingGem == null)
                         continue;
 
+                    FallMove fallMove = new FallMove()
+                    {
+                        gem = fallingGem,
+                        currentPos = fallingGem.transform.position,
+                        targetPos = new Vector3(x, -y),
+                    };
+
                     grid.Set(x, y, fallingGem);
                     grid.Set(x, sourceY, null);
 
                     fallingGem.SetGridPos(x, y);
-                    fallingGem.transform.position =
-                        new Vector2(x, -y);
 
+                    fallMoves.Add(fallMove);
                     break;
                 }
             }
         }
+        return fallMoves;
     }
 }
