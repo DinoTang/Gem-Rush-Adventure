9fad52a198e82ba1cbafeb423f27052d65078ef8
DinoTang <quockhanhtanghuynh@gmail.com>
2026-05-14 21:10:20 +0700

    Design again gameplay

--- Assets/_Scripts/_Core/Board/SpawnResolver.cs
+++ Assets/_Scripts/_Core/Board/SpawnResolver.cs
@@ -1,31 +1,31 @@
-using System;
-using System.Collections.Generic;
+// using System;
+// using System.Collections.Generic;
 
-public class SpawnResolver
-{
-    static Random random = new Random();
-    static PieceType[] types =
-    {
-        PieceType.Red,
-        PieceType.Blue,
-        PieceType.Green,
-        PieceType.Yellow,
-        PieceType.Purple
-    };
-    static PieceType GetRandomPieceType()
-    {
-        return types[new Random().Next(types.Length)];
-    }
+// public class SpawnResolver
+// {
+//     static Random random = new Random();
+//     static PieceType[] types =
+//     {
+//         PieceType.Red,
+//         PieceType.Blue,
+//         PieceType.Green,
+//         PieceType.Yellow,
+//         PieceType.Purple
+//     };
+//     static PieceType GetRandomPieceType()
+//     {
+//         return types[new Random().Next(types.Length)];
+//     }
 
-    // public void FillEmptyCells(GridModel grid)
-    // {
-    //     for (int x = 0; x < grid.Width; x++)
-    //     {
-    //         for (int y = 0; y < grid.Height; y++)
-    //         {
-    //             if (grid.Get(x, y).pieceType == PieceType.None)
-    //                 grid.Set(x, y, new Piece(GetRandomPieceType()));
-    //         }
-    //     }
-    // }
-}
+//     public void FillEmptyCells(GridModel grid)
+//     {
+//         for (int x = 0; x < grid.Width; x++)
+//         {
+//             for (int y = 0; y < grid.Height; y++)
+//             {
+//                 if (grid.Get(x, y).pieceType == PieceType.None)
+//                     grid.Set(x, y, new Piece(GetRandomPieceType()));
+//             }
+//         }
+//     }
+// }
