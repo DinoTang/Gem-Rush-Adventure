c507f2155a0c83e4f5fda0ca45ce40e159753f61
DinoTang <quockhanhtanghuynh@gmail.com>
2026-05-14 21:10:30 +0700

    feat(match3): implement special gem transformation system

--- Assets/_Scripts/_Core/Match/MatchResult.cs
+++ Assets/_Scripts/_Core/Match/MatchResult.cs
@@ -1,6 +1,11 @@
 using System.Collections.Generic;
-
+public enum MatchDirection
+{
+    Horizontal,
+    Vertical
+}
 public class MatchResult
 {
     public List<(int x, int y)> Cells = new();
+    public MatchDirection MatchDirection;
 }
